0518f350321d584a16f3cf05e874a38b6fa19a13
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-02 21:19:42 -0300

    cambios esteticos pagina bienvenida

--- aparece.js
+++ aparece.js
@@ -1,12 +1,12 @@
 
 /*Funcion para cuando haga scroll aparezcan las cosas en la pagina principal*/
 
-(function(){
+(function () {
 
     /*defino en variables el contenedor que quiero hacer que aparezca 
     y una variable con el tamano de la pantalla del navegador*/
 
-    var contenedorImgyTxt = document.getElementById('scroll');
+    var cajatarjetas = document.getElementById('scroll');
     var alturaPantalla = window.innerHeight;
 
 
@@ -15,29 +15,34 @@
     si no quitamos la clase .visible*/
 
 
-    var aparece1 = function(){
+    var aparece1 = function () {
 
-        if(contenedorImgyTxt.getBoundingClientRect().top < alturaPantalla){
-            contenedorImgyTxt.classList.add('visible');
-           }else{
-            contenedorImgyTxt.classList.remove('visible')
-           }
+       
+
+        if (cajatarjetas.getBoundingClientRect().top < alturaPantalla) {
+            cajatarjetas.classList.add('visible');
+        } else {
+            cajatarjetas.classList.remove('visible')
+        }
 
     }
 
 
 
 
-    var aparece = function(){
+    var aparece = function () {
         aparece1();
-        
+
     }
 
+   
 
     /*evento para cuando detecte el scroll active la funcion*/
 
 
-    window.addEventListener('scroll', aparece)
+   
+
+    window.addEventListener('scroll',aparece)
 
 
 
