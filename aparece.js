fc76e480e452dffcd8d6250c1c3eeae7826cb99d
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-06 13:35:45 -0300

    sdasd

--- aparece.js
+++ aparece.js
@@ -1,51 +1,66 @@
 
 /*Funcion para cuando haga scroll aparezcan las cosas en la pagina principal*/
 
-(function () {
-
-    /*defino en variables el contenedor que quiero hacer que aparezca 
-    y una variable con el tamano de la pantalla del navegador*/
-
-    var cajatarjetas = document.getElementById('scroll');
-    var alturaPantalla = window.innerHeight;
 
 
-    /*Un if para comprobar si la partre superio de nuestro contenedor 
-    es menor que la altura de la pantalla aplicamos la clase .visible
-    si no quitamos la clase .visible*/
+/*defino en variables el contenedor que quiero hacer que aparezca 
+y una variable con el tamano de la pantalla del navegador*/
+
+var cajatarjetas = document.getElementById('scroll');
+var textoprincipal = document.getElementById('textoprincipal');
 
 
-    var aparece1 = function () {
+var alturaPantalla = window.innerHeight;
 
-       
 
-        if (cajatarjetas.getBoundingClientRect().top < alturaPantalla) {
-            cajatarjetas.classList.add('visible');
-        } else {
-            cajatarjetas.classList.remove('visible')
-        }
+/*Un if para comprobar si la partre superio de nuestro contenedor 
+es menor que la altura de la pantalla aplicamos la clase .visible
+si no quitamos la clase .visible*/
 
+
+var aparece1 = function () {
+
+
+
+    if (cajatarjetas.getBoundingClientRect().top < alturaPantalla) {
+        cajatarjetas.classList.add('visible');
+    } else {
+        cajatarjetas.classList.remove('visible')
     }
 
-
-
-
-    var aparece = function () {
-        aparece1();
-
-    }
-
-   
-
-    /*evento para cuando detecte el scroll active la funcion*/
-
-
-   
-
-    window.addEventListener('scroll',aparece)
-    window.addEventListener('focus')
+}
 
 
 
 
-}())
+var aparece = function () {
+    aparece1();
+
+
+}
+
+
+
+/*evento para cuando detecte el scroll active la funcion*/
+
+
+
+
+window.addEventListener('scroll', aparece)
+
+
+var aparecetodo = function () {
+    setTimeout(function () {
+        textoprincipal.classList.add('visible');
+        
+    }, 2000);
+
+}
+
+aparecetodo();
+
+
+
+
+
+
