21202c611e00f68b0c2302207be3394166503731
PascualiDarius12 <dario12bifa@gmail.com>
2023-05-25 02:43:07 -0300

    sdasd

--- formulario/Formulario.js
+++ formulario/Formulario.js
@@ -1,65 +1,98 @@
 
 
-    var formulario = document.getElementById('formulario'),
+   /* var formulario = document.getElementById('formulario'),
         elementos = formulario.elementos,
         boton = document.getElementById('boton')
 
-  /*funciones para validar cada input:*/
+    let fnombre = document.forms["formu"]["nombre"];
 
-    var validarNombre = function(e){
-        if(formulario.nombre.value == 0){
-           alert("Debe completar el campo Nombre y Apellido");
-           e.preventDefault();
+  /*funciones para validar cada input:
+
+    var validarNombre = function(){
+        if(fnombre.value == 0){
+          // alert("Debe completar el campo Nombre y Apellido");
+          fnombre.st
+          
+           
         }
 
     };
-    var validarDni = function(e){
+    var validarDni = function(){
         if(formulario.dni.value == 0){
-           alert("Debe completar el campo Dni");
-           e.preventDefault();
+          // alert("Debe completar el campo Dni");
+           
         }
 
     };
-    var validarTelefono = function(e){
+    var validarTelefono = function(){
         if(formulario.telefono.value == 0){
-           alert("Debe completar el campo Telefono");
-           e.preventDefault();
+         //  alert("Debe completar el campo Telefono");
+          
         }
 
     };
-    var validarEspecialidad= function(e){
+    var validarEspecialidad= function(){
         if(formulario.especialidad.value == 0){
-           alert("Debe elegir una Especialidad");
-           e.preventDefault();
+          // alert("Debe elegir una Especialidad");
+          
         }
 
     };
-    var validarObraSocial= function(e){
+    var validarObraSocial= function(){
         if(formulario.obraSocial.value == 0){
-           alert("Debe elegir una Obra Social");
-           e.preventDefault();
+          // alert("Debe elegir una Obra Social");
+          
         }
 
     };
 
 
 
-  /*la funcion validar tiene todas las funciones de vailidacion de cada input adentro:*/
+  la funcion validar tiene todas las funciones de vailidacion de cada input adentro:
 
-   var validar = function(e){
+   var validar = function(){
 
-    validarNombre(e);
-    validarDni(e);
-    validarTelefono(e);
-    validarEspecialidad(e);
-    validarObraSocial(e);
+    validarNombre();
+    validarDni();
+    validarTelefono();
+    validarEspecialidad();
+    validarObraSocial();
 
    };
 
-   /*Para cuando pise enviar se inicie la funcion validar:*/
+   Para cuando pise enviar se inicie la funcion validar:
 
-   formulario.addEventListener('submit', validar)
+   formulario.addEventListener('submit', validar) */
+   function validateForm() {
+    let fnombre = document.forms["formu"]["nombre"];
+    let fdni = document.forms["formu"]["dni"];
+    let ftelefono = document.forms["formu"]["telefono"];
 
+    if (fnombre.value == "") {
+      fnombre.style.borderBottom = "2px solid red"
+      
+    }else{
+        fnombre.style.borderBottom = "2px solid #01c1012e" 
+    }
+
+    
+    if (fdni.value == "") {
+      fdni.style.borderBottom = "2px solid red"
+      
+    }else{
+        fdni.style.borderBottom = "2px solid #01c1012e"
+    }
+    
+    if (ftelefono.value == "") {
+      ftelefono.style.borderBottom = "2px solid red"
+      
+    }else{
+        ftelefono.style.borderBottom = "2px solid #01c1012e"
+    }
+
+    return false;
+
+  }
    
   
 
